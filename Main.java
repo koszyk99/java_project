889f16e03c4e5a5d74ccd730aeb072c6d4119795
koszyk99 <krzysztof_koszyk@interia.eu>
2026-08-24 19:44:57 +0200

    feat: for and for-each loops

--- Main.java
+++ Main.java
@@ -8,8 +8,28 @@ public class Main {
         tasks.add("Buy milk");
         tasks.add("Buy water");
         tasks.add("Buy ham");
+        // tasks.remove(1); //
 
-        System.out.println(tasks.size());
-        System.out.println(tasks.get(0));
+        // System.out.println(tasks.size()); //
+        // System.out.println(tasks.get(1)); //
+        // System.out.println(tasks.contains("Buy bread")); //
+        // System.out.println(tasks.contains("Buy milk")); //
+        for (String task : tasks) {
+            System.out.println(task);
+        }
+
+        List<Integer> numbers = new ArrayList<>();
+
+        numbers.add(0);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+
+        int sum = 0;
+        for (int i = 0; i < numbers.size(); i++) {
+            sum = sum + numbers.get(i);
+        }
+        System.out.println(sum);
     }
 }
