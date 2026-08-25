45fae50f10c38a28520ca369e1d55924257ebfc7
koszyk99 <krzysztof_koszyk@interia.eu>
2026-08-25 18:11:47 +0200

    feat: create method printAll

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) {
+    /* public static void main(String[] args) {
         List<String> tasks = new ArrayList<>();
         
         tasks.add("Buy milk");
@@ -31,5 +31,35 @@ public class Main {
             sum = sum + numbers.get(i);
         }
         System.out.println(sum);
+    }*/
+    
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+
+        int result = sumList(numbers);
+        System.out.println(result);
+
+        List<String> tasks = new ArrayList<>();
+        tasks.add("Buy milk");
+        tasks.add("Buy water");
+
+        printAll(tasks);
+    }
+
+    public static int sumList(List<Integer> list) {
+        int sum = 0;
+        for(int i = 0; i < list.size(); i++) {
+            sum = sum + list.get(i);
+        }
+
+        return sum;
+    }
+
+    public static void printAll(List<String> tasks) {
+        for(String task : tasks) {
+            System.out.println(task);
+        }
     }
 }
